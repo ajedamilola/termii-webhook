675caee5bd0aa61e2896053f6e27e66212b05a7c
Aje Damilola <88620584+ajedamilola@users.noreply.github.com>
2024-07-05 22:29:11 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import cors from "cors"
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.bodyParser())
 app.use((req, res) => {
     console.log({
         headers: req.headers,
